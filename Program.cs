a55f9ecbfb65dd8b787d1dd562124aa6b5b022db
Daniel Echeverri <echeverri121@gmail.com>
2024-01-21 17:39:33 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
+});
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
