1a368bcfcd0d2317e871ca7fb4c7359a85fd204a
Daniel Echeverri <echeverri121@gmail.com>
2024-01-21 17:40:15 -0500

    Setting up swagger

--- Program.cs
+++ Program.cs
@@ -8,6 +8,12 @@ builder.Services.AddSwaggerGen(c =>
 });
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+   c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+});
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
